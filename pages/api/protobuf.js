1f797173081330a2b5ba8390d2e192ccb22a4689
fujinwen <450514343@qq.com>
2020-01-15 18:08:01 +0800

    'log'

--- pages/api/protobuf.js
+++ pages/api/protobuf.js
@@ -1,18 +1,17 @@
 let root = require('../../data/scripts/user.js')
 export default (req, res) => {
-  console.log('api/protobuf.js', root)
   let auser = root.userpackage.User.create({
     name: 'zhangshan',
     id: 12345,
     email: 'zhangshan@abc.com'
   });
-  console.log('api/protobuf.js auser', auser)
+  // console.log('api/protobuf.js auser', auser)
   let buffer = root.userpackage.User.encode(auser).finish();
-  console.log('api/protobuf.js buffer', buffer)
+  // console.log('api/protobuf.js buffer', buffer)
   let message = root.userpackage.User.decode(buffer)
-  console.log('api/protobuf.js message', message)
+  // console.log('api/protobuf.js message', message)
   let object = root.userpackage.User.toObject(message)
-  console.log('api/protobuf.js object', object)
+  // console.log('api/protobuf.js object', object)
   // response.setHeader(name, value)
   res.writeHead(200, {
     'Access-Control-Allow-Origin': '*',
